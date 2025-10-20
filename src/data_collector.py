89b3513ee243c9d748df376d90ba14bee8fdcf0f
Landen <landenheine17@gmail.com>
2025-10-19 23:58:13 -0600

    added main and card templates
    Code is very messy right now, but I switched the entire project to use template matching and OH MY LORD, it works a million times better, my only concern is  the fact that it doesn't match by size as well. I learned a lot about image recognition and I don't regret "wasting" all that time with image recognition. but now I be running with grayscale template matching.

--- src/data_collector.py
+++ src/data_collector.py
@@ -6,27 +6,32 @@ from utils import capture_window
 
 # Settings
 WINDOW_TITLE = "Bluestacks"
-SAVE_DIR = "data/shop_data/raw_shop_data"
+SAVE_DIR = "data/shop_data/raw_shop_data/data_set3"
 SHOP_ROI = (470, 941, 787, 1085)  # (x1, y1, x2, y2)
 FIELD_ROI = (477, 543, 833, 765)
+SHOP_ROI = (402, 815, 678, 925)
 
 # Make sure the folder exists
 os.makedirs(SAVE_DIR, exist_ok=True)
+
 
 def screenshot_capture():
     counter = 0
     print("Press 'S' to capture the shop panel (grayscale). Press 'Q' to quit.")
 
     while True:
-        # Check if "S" is pressed
+        # Check if "S" is presseds
         if keyboard.is_pressed("s"):
             frame = capture_window(WINDOW_TITLE)
+
+            cv2.imshow("window", frame)
 
             # Crop to shop region
             x1, y1, x2, y2 = SHOP_ROI
             shop_crop = frame[y1:y2, x1:x2]
 
             # Convert to grayscale
+            # currently not in use
             gray_crop = cv2.cvtColor(shop_crop, cv2.COLOR_BGR2GRAY)
 
             # Filename: timestamp + counter
@@ -35,8 +40,8 @@ def screenshot_capture():
                 f"shop_{int(time.time())}_{counter}.png"
             )
 
-            cv2.imwrite(filename, gray_crop)
-            print(f"[+] Saved shop screenshot: {filename}")
+            cv2.imwrite(filename, shop_crop)
+            print(f"[+] Saved shop sscreenshot: {filename}")
 
             counter += 1
             time.sleep(0.3)  # Prevent multiple triggers
--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,11 @@ def get_window_rect(title_substr):
         raise RuntimeError("Window not found")
     win = wins[0]
     left, top = win.topleft
+    a = {
+        "width": win.width,
+        "height": win.height
+    }
+    print(a)
     return {
         "left": left,
         "top": top,
@@ -26,9 +31,8 @@ def capture_window(title_substr):
             "height": region["height"]
         }
 
-        img = sct.grab(monitor)
-        frame = np.frombuffer(img.rgb, dtype=np.uint8).copy()
-        frame = frame.reshape((img.height, img.width, 3))
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        
-        return frame
+        screenshot = sct.grab(monitor)
+        img = np.array(screenshot)
+        #gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+         
+        return img
